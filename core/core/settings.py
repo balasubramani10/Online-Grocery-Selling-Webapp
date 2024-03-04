1028ac85b388594666c8e1416453846e537151c4
Balasubramani Muthukrishnan <141347193+balasubramani10@users.noreply.github.com>
2024-03-04 14:53:10 +0530

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!6hg+_@gqnk(u@gv&jm^=#!rok6n0jltct&_dw2nfs-jw8@w5+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
